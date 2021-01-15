2615ec73fe579910deee850063626a919f04619c
developer <developer@email.com>
2021-01-15 22:10:22 +0100

    finish design Home-Page

--- js/script.js
+++ js/script.js
@@ -2,18 +2,19 @@
 // Code Scroll Hash 800 ms
 
 $(document).ready(function(){
-  $("a.scroll").on('click', function(event){
+  
+  // $("a.scroll").on('click', function(event){
 
-      var hash = this.hash;
+  //     var hash = this.hash;
 
-      $('html,body').animate({scrollTop: $(hash).offset().top},800,function(){})
-  });
+  //     $('html,body').animate({scrollTop: $(hash).offset().top},800,function(){})
+  // });
 
       //للعداد في قسم إحصائيات الشركة
       $('.timer').countTo();
 
   // Activate lib AOS Animate
-  // AOS.init();
+    AOS.init();
 });
 
 
